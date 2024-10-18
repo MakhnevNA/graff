568476c0f3c7c11354a0a1156064f896389e3e1f
Nikita <nikitka.makhnev@mail.ru>
2024-10-18 18:54:08 +0500

    Обновил данные для подключения к БД

--- server/src/db.ts
+++ server/src/db.ts
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT ? parseInt( process.env.PG_PORT) : 3001,
-    database: process.env.PG_DB_NAME,
+    user: process.env.SUPABASE_USER,
+    password: process.env.SUPABASE_PASSWORD,
+    host: process.env.SUPABASE_HOST,
+    port: parseInt(process.env.SUPABASE_PORT!),
+    database: process.env.SUPABASE_DB_NAME
 });
 
 export default pool;
